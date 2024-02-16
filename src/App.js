5f5f8b8935e92c60ded6221e8737247ac420d891
mrduman <omer.duman@gmx.de>
2024-02-16 17:15:09 +0100

    Initial componentes and their routes created

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import { Box } from "@mui/material";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Feed from "./components/Feed";
+import Navbar from "./components/Navbar";
+import VideoDetail from "./components/VideoDetail";
+import ChannelDetail from "./components/ChannelDetail";
+import SearchFeed from "./components/SearchFeed";
+
 function App() {
-  return <div className="App">Hello world</div>;
+  return (
+    <BrowserRouter>
+      <Box sx={{ bgcolor: "#000" }}>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact element={<Feed />} />
+          <Route path="/video/:id" element={<VideoDetail />} />
+          <Route path="/channel/:id" element={<ChannelDetail />} />
+          <Route path="/search/:searchTerm" element={<SearchFeed />} />
+        </Routes>
+      </Box>
+    </BrowserRouter>
+  );
 }
 
 export default App;
