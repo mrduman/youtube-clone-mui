1aa3b63caf2c107485734cc2a38177f987345233
mrduman <omer.duman@gmx.de>
2024-02-17 16:55:20 +0100

    sideabar icons and categories displayed

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,8 +1,26 @@
 import { Stack } from "@mui/material";
 import React from "react";
 
+import { categories } from "../utils/constants";
+
 const Sidebar = () => {
-  return <Stack></Stack>;
+  return (
+    <Stack
+      direction={"row"}
+      sx={{
+        overflowY: "auto",
+        height: { sx: "auto", md: "91%" },
+        flexDirection: { md: "column" },
+      }}
+    >
+      {categories.map((category) => (
+        <button>
+          <span>{category.icon}</span>
+          <span>{category.name}</span>
+        </button>
+      ))}
+    </Stack>
+  );
 };
 
 export default Sidebar;
