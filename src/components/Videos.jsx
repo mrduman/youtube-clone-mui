17f4c79cf9d513ed75fc418c5e0080f7d013ac71
mrduman <omer.duman@gmx.de>
2024-02-18 16:42:12 +0100

    Videos component created

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,7 +1,18 @@
+import { Box, Stack } from "@mui/material";
 import React from "react";
+import { ChannelDetail, VideoCard } from "./";
 
-const Videos = () => {
-  return <div>Videos</div>;
+const Videos = ({ videos }) => {
+  return (
+    <Stack>
+      {videos.map((item, index) => (
+        <Box>
+          {item.id.videoId && <VideoCard />}
+          {item.id.channelId && <ChannelDetail />}
+        </Box>
+      ))}
+    </Stack>
+  );
 };
 
 export default Videos;
